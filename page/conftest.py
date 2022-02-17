88fe18beee0fa6e72ad10a993a83c12e3159f346
wangzhipeng <11>
2022-02-17 17:42:12 +0800

    优化

--- page/conftest.py
+++ page/conftest.py
@@ -10,7 +10,7 @@ def login_fixtrue(driver):
     #登录前置操作
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ['enable-automation'])
-    driver = webdriver.Chrome(chrome_options=options,executable_path='/usr/local/bin/chromedrive')
+    driver = webdriver.Chrome(chrome_options=options,executable_path='/usr/local/bin/chromedrive.exe')
     driver.maximize_window()
     return driver
 def pytest_addoption(parser):
@@ -26,7 +26,7 @@ def driver(request):
         chrome_options.add_argument('--window-size=1920,1080')  # 设置当前窗口的宽度，高度
         chrome_options.add_argument('--headless')  # 无界面
         log.info("当前运行的操作系统为windows")
-        _driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='/usr/local/bin/chromedrive')
+        _driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='/usr/local/bin/chromedrive.exe')
     else:
         log.info('当前运行的操作系统为mac')
         chrome_options = Options()
@@ -36,7 +36,7 @@ def driver(request):
         chrome_options.add_argument('--disable-gpu')#禁用GPU硬件加速，如果软件渲染器没有就位，则GPU进程将不会启动
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--headless')  # 无界面
-        _driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='/usr/local/bin/chromedrive')
+        _driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='/usr/local/bin/chromedrive.exe')
 
     def end():
         log.info("全部用例执行完后 teardown quit dirver")
