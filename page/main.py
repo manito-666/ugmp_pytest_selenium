c897f57a79c9136338311ead266f3e5ba0c9f629
wangzhipeng <11>
2022-02-17 17:04:10 +0800

    优化

--- page/main.py
+++ page/main.py
@@ -1,9 +1,11 @@
-import os
+import os,sys
 import pytest
+Path = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(Path)[0]
+sys.path.append(rootPath)
 from util.log import log
 case_path = os.path.join(os.getcwd())
 PATH = os.path.split(os.path.realpath(__file__))[0]
-print(PATH)
 failureException = AssertionError
 
 if __name__ == '__main__':
